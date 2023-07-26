1db11767976dc23c79d40a78e89b904a8105fce9
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-07-26 18:51:35 +0100

    0x01: define `MRUCache` class as a caching system
    It uses the MRU algorithm to cache data. If the max size of the cache is
    reached, it replaces the most recently used item with the new one.

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""
+This module contains the class `MRUCache` as a caching system.
+"""
+
+from typing import Any
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class.
+    """
+
+    def __init__(self):
+        """Initializes MRUCache instances.
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key: Any, item: Any) -> None:
+        """
+        `put` uses the MRU algorithm to insert a new item with given key
+        into the cache. If the cache is full, it replaces the most recently
+        used item in the cache with the new item.
+        If key or item is None, it does nothing.
+        """
+        if key is None or item is None:
+            return
+
+        if BaseCaching.MAX_ITEMS <= 0:
+            print("Could not insert [{}:{}] as cache max size is <= 0")
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            mru_key = None
+            if len(self.queue) > 0:
+                mru_key = self.queue.pop()
+
+            if key not in self.cache_data and mru_key:
+                try:
+                    del self.cache_data[mru_key]
+                    print("DISCARD:", mru_key)
+                except KeyError:
+                    msg = "Could not insert [{}:{}] as MRU key not in cache"
+                    print(msg.format(key))
+                    return
+            elif key in self.queue:
+                self.queue.remove(key)
+            else:
+                return
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key: Any) -> Any:
+        """
+        `get` gets an item by key and maintains a queue of the keys.
+        """
+
+        if key is not None and key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+        return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    my_cache = MRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Chee-zaram")
+    my_cache.put("D", "Okeke")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
